964dd28becff4fb1fb7c6b3c8e05c2bd8e488e98
Hans-Peter Oeri <hp@oeri.ch>
2017-01-15 20:47:14 +0100

    initial import

---
+++ src/Exception/EnvironmentException.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * This file is part of KampfCaspar_SPL.
+ *
+ * KampfCaspar_SPL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * KampfCaspar_SPL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with KampfCaspar_SPL. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+namespace KampfCaspar\SPL\Exception;
+
+/** Environment Exception
+ *
+ * Thrown for all problems with the server setup
+ *
+ * @author  Hans-Peter Oeri <hp@oeri.ch>
+ * @version 0.1
+ */
+class EnvironmentException extends \LogicException {}
---
+++ src/Exception/SystemException.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * This file is part of KampfCaspar_SPL.
+ *
+ * KampfCaspar_SPL is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * KampfCaspar_SPL is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ * You should have received a copy of the GNU General Public License along
+ * with KampfCaspar_SPL. If not, see <http://www.gnu.org/licenses/>.
+ */
+namespace KampfCaspar\SPL\Exception;
+
+/** System Exception
+ *
+ * Thrown for all 'external' problems, like connection problems
+ *
+ * @author  Hans-Peter Oeri <hp@oeri.ch>
+ * @version 0.1
+ */
+class SystemException extends \RuntimeException {}
